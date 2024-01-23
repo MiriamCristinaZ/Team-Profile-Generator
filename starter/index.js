71d131c7c66fdf4065603a163689f65897dbce15
Miriam Cristina Zarzu <145344909+MiriamCristinaZ@users.noreply.github.com>
2024-01-23 20:49:48 +0000

    added functions to get user prompts and to push choices into array, function to build team and generate html based on answers

--- starter/index.js
+++ starter/index.js
@@ -13,62 +13,152 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const teamMembers = [];
 
 function startApp() {
-  createManager();
-  // Start by gathering manager info, then present menu
-}
+  function createManager() {
+    // Use inquirer to prompt for manager info, then push new Manager to teamMembers
 
-function createManager() {
-  // Use inquirer to prompt for manager info, then push new Manager to teamMembers
-  console.log("Please build your team.");
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is the team manager's name?",
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is the team manager's id?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is the team manager's email?",
-      },
-      {
-        type: "input",
-        name: "officeNumber",
-        message: "What is the team manager's office number?",
-      },
-    ])
-    .then((answers) => {
-      const manager = new Manager(
-        answers.name,
-        answers.id,
-        answers.email,
-        answers.officeNumber
-      );
-      teamMembers.push(manager);
-      createTeam();
-    });
-}
-
-function addEngineer() {
-  // Use inquirer to prompt for engineer info, then push new Engineer to teamMembers
-}
-
-function addIntern() {
-  // Use inquirer to prompt for intern info, then push new Intern to teamMembers
-}
-
-function buildTeam() {
-  // Use the render function to generate and write the HTML to 'team.html'
-  if (!fs.existsSync(OUTPUT_DIR)) {
-    fs.mkdirSync(OUTPUT_DIR);
+    console.log("Please build your team.");
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the team manager's name?",
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is the team manager's id?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is the team manager's email?",
+        },
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "What is the team manager's office number?",
+        },
+      ])
+      .then((answers) => {
+        const manager = new Manager(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.officeNumber
+        );
+        teamMembers.push(manager);
+        gatherTeamMembers();
+      });
   }
-  fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
-}
+  function gatherTeamMembers() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "menuOption",
+          message: "What would you like to do next?",
+          choices: ["Engineer", "Intern", "Finish building the team"],
+        },
+      ])
+      .then((answers) => {
+        switch (answers.menuOption) {
+          case "Engineer":
+            addEngineer();
+            break;
+          case "Intern":
+            addIntern();
+            break;
+          default:
+            buildTeam();
+        }
+      });
+  }
 
+  function addEngineer() {
+    // Use inquirer to prompt for engineer info, then push new Engineer to teamMembers
+    console.log("Add an Engineer to your team:");
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "engineerName",
+          message: "What is the engineer's name?",
+        },
+        {
+          type: "input",
+          name: "engineerId",
+          message: "What is the engineer's ID?",
+        },
+        {
+          type: "input",
+          name: "engineerEmail",
+          message: "What is the engineer's email?",
+        },
+        {
+          type: "input",
+          name: "engineerGithub",
+          message: "What is the engineer's GitHub username?",
+        },
+      ])
+      .then((answers) => {
+        const engineer = new Engineer(
+          answers.engineerName,
+          answers.engineerId,
+          answers.engineerEmail,
+          answers.engineerGithub
+        );
+        teamMembers.push(engineer);
+        gatherTeamMembers();
+      });
+  }
+
+  function addIntern() {
+    // Use inquirer to prompt for intern info, then push new Intern to teamMembers
+    console.log("Add an Intern to your team:");
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "internName",
+          message: "What is the intern's name?",
+        },
+        {
+          type: "input",
+          name: "internId",
+          message: "What is the intern's ID?",
+        },
+        {
+          type: "input",
+          name: "internEmail",
+          message: "What is the intern's email?",
+        },
+        {
+          type: "input",
+          name: "internSchool",
+          message: "Which school is the intern attending?",
+        },
+      ])
+      .then((answers) => {
+        const intern = new Intern(
+          answers.internName,
+          answers.internId,
+          answers.internEmail,
+          answers.internSchool
+        );
+        teamMembers.push(intern);
+        gatherTeamMembers();
+      });
+  }
+
+  function buildTeam() {
+    // Use the render function to generate and write the HTML to 'team.html'
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+    console.log("Succesfully built the team!");
+  }
+  createManager();
+}
 startApp();
