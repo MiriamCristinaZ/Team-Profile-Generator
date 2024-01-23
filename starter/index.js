55ed241d0bad4dcf5efdfc795bb8e3cd8f68d81a
Miriam Cristina Zarzu <145344909+MiriamCristinaZ@users.noreply.github.com>
2024-01-23 18:58:21 +0000

    added prompt for creating manager profile

--- starter/index.js
+++ starter/index.js
@@ -1,4 +1,4 @@
-TODO: Write Code to gather information about the development team members, and render the HTML file.
+//TODO: Write Code to gather information about the development team members, and render the HTML file.
 const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
@@ -13,11 +13,46 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const teamMembers = [];
 
 function startApp() {
+  createManager();
   // Start by gathering manager info, then present menu
 }
 
 function createManager() {
   // Use inquirer to prompt for manager info, then push new Manager to teamMembers
+  console.log("Please build your team.");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the team manager's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the team manager's id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the team manager's email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the team manager's office number?",
+      },
+    ])
+    .then((answers) => {
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      teamMembers.push(manager);
+      createTeam();
+    });
 }
 
 function addEngineer() {
--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,7 +1,6 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 // Engineer.js
 const Employee = require("./Employee");
-
 class Engineer extends Employee {
   constructor(name, id, email, github) {
     super(name, id, email); // Call the parent class constructor
